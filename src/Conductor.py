8be287a39de4a2b23db30d0b2a3bc06ff909d931
RandallWoodall <randalljwoodall@gmail.com>
2018-10-23 09:45:05 -0600

    start on line project.

---
+++ src/Conductor.py
@@ -0,0 +1,8 @@
+class conductor:
+    __init__(self, circ_mils, out_diam, gmr_60, amp_cap, res):
+        self.circ_mils = circ_mils
+        self.out_diam = out_diam
+        self.gmr_60 = gmr_60
+        self.amp_cap = amp_cap
+        self.res = res
+        
