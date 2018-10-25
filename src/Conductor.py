c4a9f4726cee4ebf289928fdaa3c1887cf5c0019
RandallWoodall <randalljwoodall@gmail.com>
2018-10-25 09:47:19 -0600

    Adding more conductors.

--- src/Conductor.py
+++ src/Conductor.py
@@ -1,8 +1,9 @@
 class conductor:
-    __init__(self, circ_mils, out_diam, gmr_60, amp_cap, res):
+    __init__(self, circ_mils, out_diam, gmr, r_60, amp_cap, res):
         self.circ_mils = circ_mils
         self.out_diam = out_diam
-        self.gmr_60 = gmr_60
+        self.gmr = gmr
+        self.r_60 = r_60
         self.amp_cap = amp_cap
         self.res = res
         
