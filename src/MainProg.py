f7703db96451472f5a8e6118e5e363a7741bae42
RandallWoodall <randalljwoodall@gmail.com>
2018-10-30 02:14:09 -0600

    Display results.

--- src/MainProg.py
+++ src/MainProg.py
@@ -39,8 +39,9 @@ reactance_per_km = numpy.log(Deq/DS) * 754e-4
 suseptance_per_km = 240000 * constants.pi**2 * constants.epsilon_0 / numpy.log(Deq/DSC)
 
 # Get ABCD
-z = resistance_per_km / 1000 + 1j*reactance_per_km / 1000
-y = 1j*suseptance_per_km / 1000
+z = resistance_per_km + 1j*reactance_per_km
+y = 1j*suseptance_per_km
+
 gamma = numpy.sqrt(z * y)
 Zc = numpy.sqrt(z/y)
 Beta = numpy.imag(gamma)
@@ -59,6 +60,89 @@ ABCDind = numpy.matrix([[1, 0], [1j * compInd, 1]])
 # Apply compensation
 step1 = numpy.matmul(ABCDind, ABCDcap)
 step2 = numpy.matmul(step1, ABCDline)
-step3 = numpy.matmul(step2, ABCDcap)
-ABCDfull = numpy.matmul(step3, ABCDind)
-print(ABCDfull)
+ABCDnoLoad = numpy.matmul(step2, ABCDcap)
+ABCDfullLoad = numpy.matmul(ABCDnoLoad, ABCDind)
+Afl = ABCDfullLoad[0,0]
+Bfl = ABCDfullLoad[0,1]
+Cfl = ABCDfullLoad[1,0]
+Dfl = ABCDfullLoad[1,1]
+Anl = ABCDnoLoad[0,0]
+Bnl = ABCDnoLoad[0,1]
+Cnl = ABCDnoLoad[1,0]
+Dnl = ABCDnoLoad[1,1]
+
+# Get complex power per phase
+if leadLag == 'lag':
+    S = (load * 10**6) * (powerFactor + 1j * numpy.sin(numpy.arccos(powerFactor)))
+else:
+    S = (load * 10**6) * (powerFactor - 1j * numpy.sin(numpy.arccos(powerFactor)))
+Sphase = S/3
+    
+# Get line to nuetral voltage
+Vrfl = nomVolt / numpy.sqrt(3)
+
+# Calculate line current
+Iline = numpy.conj(Sphase / Vrfl)
+
+# Calculate source voltage, current, power, and losses
+Vs = Afl * Vrfl + Bfl * Iline
+Is = Cfl * Vrfl + Dfl * Iline
+Ssfl = 3 * Vs * numpy.conj(Is)
+lossfl = numpy.real(Ssfl) - numpy.real(S)
+
+# Get no load voltage + current
+Vrnl = Vs / Anl
+Isnl = Cnl * Vrnl
+Ssnl = 3 * Vs * numpy.conj(Isnl)
+lossnl = numpy.real(Ssnl)
+
+# Calculate voltage regulation
+Regulation = (numpy.abs(Vrnl) - numpy.abs(Vrfl))/numpy.abs(Vrfl) * 100
+
+# Calculate loadability @ 35 degrees
+vMag = numpy.abs(Vrfl)
+Pmax = (vMag**2 * numpy.cos(numpy.angle(Bnl) - 35 * constants.pi/180)/numpy.abs(Bnl) 
+        - vMag**2 * numpy.real(Anl/Bnl)) * 3
+
+# Output all the desired values
+print("Receiving end voltage (kV): " + str(nomVolt))
+print("Line Length (km): " + str(lineLen))
+print("Conductors per bundle: " + str(bundleNum))
+print("Distance between lines in bundle: " + str(bundleDist))
+print("Conductor name: " + lineName)
+print("Conductor gmr (ft): " + str(conductorChoice.gmr))
+print("Conductor radius (ft): " + str(conductorChoice.out_diam / 24))
+print("AC resistance @ 50C: " + str(conductorChoice.r_60))
+print("Ampacity: " + str(conductorChoice.amp_cap))
+print("Series compensation: " + str(seriesComp * 100) + "%")
+print("Shunt compensation: " + str(shuntComp * 100) + "%")
+print("Load (MW): " + str(load))
+print("Power factor: " + str(powerFactor) + " " + leadLag)
+print("Voltage regulation: " + str(Regulation) + "%")
+print("Loadability at 35 degrees (MW): " + str(Pmax * 10**-6))
+
+if Regulation < 10 and Pmax > 1.5 * load and numpy.abs(load)/numpy.imag(Ssnl) \
+    and numpy.abs(load)/numpy.imag(Ssfl):
+        print("Performance is acceptable!")
+else:
+    print("Performance is unacceptable!")
+    
+print("Full load ABCD: ")
+print(ABCDfullLoad)
+print("Sending end voltage (kV) (fl): " + str(Vs * 10**-3))
+print("Sending end current (A) (fl): " + str(Is))
+print("Sending end power (MVA) (fl): " + str(Ssfl * 10**-6))
+print("Receiving end voltage (kV) (fl): " + str(Vrfl * 10**-6))
+print("Receiving end current (A) (fl): " + str(Iline))
+print("Receiving end power (MVA) (fl): " + str(S * 10**-6))
+print("Full load losses (MW) (fl): " + str(lossfl * 10**-6))
+
+print("No load ABCD: ")
+print(ABCDnoLoad)
+print("Sending end voltage (kV) (nl): " + str(Vs * 10**-6))
+print("Sending end current (A) (nl): " + str(Isnl))
+print("Sending end power (MVA) (nl): " + str(Ssnl * 10**-6))
+print("Receiving end voltage (kV) (nl): " + str(Vrnl * 10**-3))
+print("Receiving end current (A) (nl): 0 (disconnected)")
+print("Receiving end power (VA) (nl): 0 (disconnected)")
+print("Full load losses (MW) (nl): " + str(lossnl * 10**-6))
